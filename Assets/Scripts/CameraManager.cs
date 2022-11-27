d58c28ad548d8aec68ca0f1920658e4a909d3389
Jonathan J Quirino <75098397+jquirinoc-dev@users.noreply.github.com>
2022-11-27 15:42:22 -0600

    Activity 3 is finished

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    
+
+    public static CameraManager Instance{
+        get;
+        private set;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        if(Instance != null){
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    [SerializeField] private Camera[] _cameras;
+    [SerializeField] private int _currentCamera;
+    void Start()
+    {
+        for(int i = 0; i < _cameras.Length; i++){
+
+            if(i == _currentCamera){
+                _cameras[i].gameObject.SetActive(true);
+            } else {
+                _cameras[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            DeactivateAll();
+        }
+    }
+
+    public void NextCamera(){
+        _cameras[_currentCamera].gameObject.SetActive(false);
+        _currentCamera++;
+        _currentCamera %= _cameras.Length;
+        _cameras[_currentCamera].gameObject.SetActive(true);
+    }
+
+    public void DeactivateAll(){
+        for(int i = 0; i < _cameras.Length; i++){
+            
+            _cameras[i].gameObject.SetActive(false);
+            
+        }
     }
 }
